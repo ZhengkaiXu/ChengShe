ffa571cd68af8ff2c737417fea1853f993b833af
ZhengkaiXu <695378091@qq.com>
2024-03-25 23:20:58 +0800

    2.1

---
+++ 第2章/2.1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by xuzhengkai on 2024/3/25.
+//
+#include <iostream> // 预编译命令
+#include <cmath> // 预编译命令
+using namespace  std;
+
+int main()  // 主函数
+{   // 主函数开始
+    // 计算三角函数
+    // 计算三角函数式并输出
+    cout << sin(20.0/180*3.14159) +
+    cos(20.0/180*3.14159) -
+    cos(10.0/180*3.14159) /
+    tan(10.0/180*3.14159)
+    << endl;
+    return 0;
+} // 主函数结束
