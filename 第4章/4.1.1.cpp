af4ee92060c4a9edb9229becedeae05b2f19b368
ZhengkaiXu <695378091@qq.com>
2024-06-09 18:01:25 +0800

    4.1.1

---
+++ 第4章/4.1.1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ZhengkaiXu on 2024/6/9.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "3>2 的逻辑值是" << (3 > 2) << ", 1为真。 " << endl;
+    cout << "3>=2 的逻辑值是" << (3 >= 2) << ", 1为真。 " << endl;
+    cout << "3==2 的逻辑值是" << (3 == 2) << ", 0为假。 " << endl;
+    cout << "3<2 的逻辑值是" << (3 < 2) << ", 0为假。 " << endl;
+    cout << "3<=2 的逻辑值是" << (3 <= 2) << ", 0为假。 " << endl;
+    cout << "3!=2 的逻辑值是" << (3 != 2) <<  ", 1为真。 " << endl;
+    return 0;
+}
