afad84f0dbdc63049f223874724286becbb9e223
ZhengkaiXu <695378091@qq.com>
2024-06-23 23:55:24 +0800

    0623

---
+++ 第4章/4.6.3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ZhengkaiXu on 2024/6/23.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int g = 0; // 定义变量为整数类型,初始化为0表示无解
+    for (int k = 0; k < 4; k = k + 1) { // k既是循环控制变量，也表示第k个人
+        if (((k != 0) + (k == 2) + (k == 3) + (k != 3) == 3)) {
+
+        }
+    }
+}
